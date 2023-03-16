78d1abdb06e86023c3b2c3df481515a139b17ca6
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-16 21:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+async function getData(callback) {
+try {
+const response = await fetch(url);
+const data = await response.json();
+callback(data.title);
+} catch (error) {
+console.error(error);
+}
+}
 
+const outputDiv = document.getElementById("output");
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+getData((title) => {
+outputDiv.innerText = title;
+});
+});
 //your JS code here. If required.
